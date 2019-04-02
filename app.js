652c3704f408d9ec7f257c912c7ac8cb8941f5e4
Koojahyun <kjh930819@naver.com>
2019-04-02 22:09:38 +0900

    made Middleware

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import routes from "./routes";
+import { localMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
 
 const app = express();
 
@@ -16,15 +17,16 @@ const app = express();
 // };
 // app.use(betweenHome);
 
+app.use(helmet());
 app.set("view engine", "pug");
-
 app.use(cookieParser());
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(localMiddleware)
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
