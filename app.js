1c530ed5db9796959389bfc50bca34a4e5698dc0
Koojahyun <kjh930819@naver.com>
2019-04-02 22:28:31 +0900

    controlllers

--- app.js
+++ app.js
@@ -26,10 +26,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
-app.use(localMiddleware)
+app.use(localMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
-app.use(routes.videos, videoRouter);
+//app.use(routes.videos, videoRouter);
 
 export default app;
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,6 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.render("search");
-export const videos = (req, res) => res.render("video");
-export const upload = (req, res) => res.render("upload");
-export const videoDetail = (req, res) => res.render("videoDetail");
-export const editVideo = (req, res) => res.render("editVideo");
-export const deleteVideo = (req, res) => res.render("deleteVideo");
+export const home = (req, res) => res.render("home", { pageTitle: "HOME" });
+export const search = (req, res) => res.render("search", { pageTitle: "SEARCH" });
+export const upload = (req, res) => res.render("upload", { pageTitle: "UPLOAD" });
+export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "VIDEODETAIL" });
+export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "EDITVIDEO" });
+export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "DELETEVIDEO" });
