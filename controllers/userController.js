a51fc74b8ef72b1fc694203b3d44738cced89817
Koojahyun <kjh930819@naver.com>
2019-04-12 19:18:23 +0900

    homecontroller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,24 @@
-export const join = (req, res) => res.render("join", { pageTitle: "JOIN" });
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "JOIN" });
+};
+
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, password2 }
+  } = req;
+  if (password !== password2) {
+    //alert Password was wrong
+    res.status(400);
+    res.render("join", { pageTitle: "JOIN" });
+  } else {
+    //todo : register User
+    //todo : log user in
+    res.redirect(routes.home);
+  }
+};
+
 export const login = (req, res) => res.render("login", { pageTitle: "LOGIN" });
 export const logout = (req, res) => res.render("logout", { pageTitle: "LOGOUT" });
 export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "USERDETAIL" });
