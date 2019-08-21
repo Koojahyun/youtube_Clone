5144241a7ede936c31c16a3d68d0aab3f7d87758
Koojahyun <kjh930819@naver.com>
2019-08-21 23:21:25 +0900

    creating db

--- db.js
+++ db.js
@@ -1,58 +1,15 @@
-export const videos = [
-  {
-    id: 1123,
-    title: "good",
-    description: "This is good",
-    views: 24,
-    description: "This is good",
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 123413,
-      name: "KOOJA",
-      email: "kjh@nav.com"
-    }
-  },
-  {
-    id: 1235598,
-    title: "cool",
-    description: "This is good",
-    views: 24,
-    description: "This is good",
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 123413,
-      name: "KOOJA",
-      email: "kjh@nav.com"
-    }
-  },
-  {
-    id: 12128,
-    title: "super",
-    description: "This is good",
-    views: 24,
-    description: "This is good",
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 123413,
-      name: "KOOJA",
-      email: "kjh@nav.com"
-    }
-  },
-  {
-    id: 23458,
-    title: "nice",
-    description: "This is good",
-    views: 24,
-    description: "This is good",
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 123413,
-      name: "KOOJA",
-      email: "kjh@nav.com"
-    }
-  }
-];
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Conneted to DB");
+const handleError = error => console.log(`❌ Error on DB Connection:${error}`);
+db.once("open", handleOpen);
+db.on("error", handleError);
