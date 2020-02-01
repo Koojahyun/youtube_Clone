b79442083a95ce73af6f44c83749a6a17c3eb74d
Koojahyun <kjh930819@naver.com>
2020-02-01 19:43:53 +0900

    Login

--- app.js
+++ app.js
@@ -2,37 +2,34 @@ import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
-// import bodyParser from "body-parser";
-import routes from "./routes";
+import bodyParser from "body-parser";
+import passport from "passport";
 import { localMiddleware } from "./middlewares";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
+import "./passport";
 
 const app = express();
-
-// const betweenHome = (req, res, next) => {
-//   console.log("Between");
-//   next();
-// };
-// app.use(betweenHome);
 
 app.use(helmet());
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"));
 app.use("/static", express.static("static"));
 app.use(cookieParser());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(localMiddleware);
 
 app.use(routes.users, userRouter);
 app.use(routes.home, globalRouter);
 app.use(routes.videos, videoRouter);
-//app.use(routes.videos, videoRouter);
 
 export default app;
