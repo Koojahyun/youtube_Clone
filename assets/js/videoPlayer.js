601af2402a1f24420b0061cba45c81d311e8a18c
Koojahyun <kjh930819@naver.com>
2020-02-04 13:35:33 +0900

    full screen

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -2,6 +2,7 @@ const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
 const volumeBtn = document.getElementById("jsVolumeButton");
+const screenBtn = document.getElementById("jsFullScreen");
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
@@ -23,9 +24,42 @@ function handleVolumeClick() {
   }
 }
 
+function exitFullScreen() {
+  screenBtn.innerHTML = '<i class="fas fa-expand"></i>';
+  screenBtn.addEventListener("click", goFullScreen);
+  const agent = navigator.userAgent.toLowerCase();
+  if (agent.indexOf("msie") !== -1) {
+    //익스플로러인지 체크
+    document.msExitFullscreen();
+  } else if (agent.indexOf("chrome") !== -1) {
+    document.webkitExitFullscreen();
+  } else if (agent.indexOf("safari") !== -1) {
+    document.webkitExitFullscreen();
+  } else if (agent.indexOf("firefox") !== -1) {
+    document.exitFullscreen();
+  }
+}
+
+function goFullScreen() {
+  const agent = navigator.userAgent.toLowerCase();
+  if (agent.indexOf("msie") !== -1) {
+    videoContainer.msRequestFullscreen();
+  } else if (agent.indexOf("chrome") !== -1) {
+    videoContainer.webkitRequestFullscreen();
+  } else if (agent.indexOf("safari") !== -1) {
+    videoContainer.webkitRequestFullscreen();
+  } else if (agent.indexOf("firefox") !== -1) {
+    videoContainer.requestFullscreen();
+  }
+  screenBtn.innerHTML = '<i class="fas fa-compress"></i>';
+  screenBtn.removeEventListener("click", goFullScreen);
+  screenBtn.addEventListener("click", exitFullScreen);
+}
+
 function init() {
   playBtn.addEventListener("click", handlePlayClick);
   volumeBtn.addEventListener("click", handleVolumeClick);
+  screenBtn.addEventListener("click", goFullScreen);
 }
 
 if (videoContainer) {
